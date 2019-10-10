001860f5dd855930c19fa1bc0caa3cfc6c48d9e9
Administrador <Administrador@MOTOREX.INT>
2019-10-10 13:19:02 -0500

    Creando conexion al servidor

--- IMportaciones/Form1.Designer.cs
+++ IMportaciones/Form1.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_back_panel = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_file_veritrade = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tubería de Cobre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_file_veritrade);
+            this.panel1.Controls.Add(this.btn_back_panel);
+            this.panel1.Location = new System.Drawing.Point(28, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(554, 417);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_back_panel
+            // 
+            this.btn_back_panel.Location = new System.Drawing.Point(14, 363);
+            this.btn_back_panel.Name = "btn_back_panel";
+            this.btn_back_panel.Size = new System.Drawing.Size(88, 39);
+            this.btn_back_panel.TabIndex = 0;
+            this.btn_back_panel.Text = "Volver al Panel Principal";
+            this.btn_back_panel.UseVisualStyleBackColor = true;
+            this.btn_back_panel.Click += new System.EventHandler(this.btn_back_panel_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_file_veritrade
+            // 
+            this.btn_file_veritrade.Location = new System.Drawing.Point(14, 14);
+            this.btn_file_veritrade.Name = "btn_file_veritrade";
+            this.btn_file_veritrade.Size = new System.Drawing.Size(75, 45);
+            this.btn_file_veritrade.TabIndex = 1;
+            this.btn_file_veritrade.Text = "Archivo Veritrade";
+            this.btn_file_veritrade.UseVisualStyleBackColor = true;
+            this.btn_file_veritrade.Click += new System.EventHandler(this.btn_file_veritrade_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 537);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_back_panel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_file_veritrade;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- IMportaciones/Form1.cs
+++ IMportaciones/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Globalization;
+using System.Reflection;
+using System.Data.SqlClient;
+
 
 namespace IMportaciones
 {
@@ -15,6 +20,81 @@ namespace IMportaciones
         public Form1()
         {
             InitializeComponent();
+
+            panel1.Visible = false;
+        }
+
+        string nombre_ext = " ";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Persiana_Abierta();
+        }
+
+        private void Persiana_Abierta()
+        {
+            panel1.Visible = true;
+            //panel1.Height = 0;
+            
+        }
+
+        private void Persiana_Cerrada()
+        {
+            panel1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_back_panel_Click(object sender, EventArgs e)
+        {
+            Persiana_Cerrada();
+        }
+
+        private void btn_file_veritrade_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Archivo Excel|*.xls;*xlsx;*xlsm";
+
+            if(backgroundWorker1.IsBusy != true)
+            {
+
+                if(openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    Console.WriteLine("Exito");
+
+                    nombre_ext = System.Environment.MachineName + (DateTime.Now.ToString("_ddMMyyy_hhmmss_")) + openFileDialog1.SafeFileName;
+                    System.IO.File.Copy(openFileDialog1.FileName, @"\\srv0005\Veritrade\" + nombre_ext, true);
+
+                    label1.Text = openFileDialog1.FileName;
+
+                    backgroundWorker1.RunWorkerAsync();
+
+                }
+            }
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Subir_ExcelVeritrade_a_Tb_Temporal();
+        }
+
+
+
+        private void Subir_ExcelVeritrade_a_Tb_Temporal()
+        {
+            Class1 servidor = new Class1();
+            string dor = servidor.servidor();
+            string db = servidor.Database();
+            string username = servidor.UID();
+            string pwd = servidor.PWD();
+
+            Console.WriteLine(dor);
+            SqlConnection sqlConn = new SqlConnection("Data Source ="+dor+"; Initial Catalog="+db+"; UID="+username+"; PWD="+pwd);
+
         }
     }
 }
